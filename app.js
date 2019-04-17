f2f35dbe435bacd4cd6c5633de3c1b896cb202a6
HVC <hvc_02>
2019-04-17 19:08:18 +0530

    mongo atlas added

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ var commentRoutes     = require("./routes/comments"),
     itemRoutes  = require("./routes/items"),
     indexRoutes        = require("./routes/index")   
  
-mongoose.connect("mongodb://localhost/yelpfood");    
+//mongoose.connect("mongodb://localhost/yelpfood"); 
+mongoose.connect("mongodb+srv://admin-hvc:Test123@cluster0-xhyav.mongodb.net/yelpfood");    
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine" , "ejs");
 app.use(express.static(__dirname + "/public"));
